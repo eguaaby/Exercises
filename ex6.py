e58223c2d5a5842f4e6812dae91704edd149d878
Abhijay Gupta <abhijay.gupta@ericsson.com>
2017-08-16 11:53:05 -0400

    ex6: palindrome exercise completed

---
+++ ex6.py
@@ -0,0 +1,14 @@
+def ex6():
+    user_input = raw_input("Please enter a word: ")
+    palindrome = True
+    wordLength = len(user_input)
+    for i in range(0, wordLength/2 + 1):
+        if user_input[i] != user_input[wordLength-1-i]:
+            palindrome = False
+
+    if palindrome:
+        print "The word is a palindrome!!"
+    else:
+        print "The word is not a palindrome"
+
+ex6()
