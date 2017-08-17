3fdc98c42f8726242014500a44eeb955ef27423f
Abhijay Gupta <abhijay.gupta@ericsson.com>
2017-08-17 11:21:43 -0400

    starred line for list comprehension

--- ex7.py
+++ ex7.py
@@ -2,7 +2,10 @@
 # from a list and put them in a new list
 def ex7():
     a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    # list comprehension
+    # **************************
     evenNums = [number for number in a if number % 2 == 0]
+    # **************************
     print evenNums
 
 ex7()
