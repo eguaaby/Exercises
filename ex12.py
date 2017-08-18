456cdeb9e72ba4bd482c5c132195da724549e697
Abhijay Gupta <abhijay.gupta@ericsson.com>
2017-08-18 13:40:44 -0400

    ex12: return a new list containing first and last elements of given list

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# takes a list of numbers and makes a new list
+# containing only first and last elements of the list
+
+def getElements():
+    a = [5, 10, 15, 20, 25]
+    b = [a[0], a[-1]]
+    print b
+
+getElements()
