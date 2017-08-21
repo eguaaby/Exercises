470893fba93062038cb95ab894cad45366a8cb7c
Abhijay Gupta <abhijay.gupta@ericsson.com>
2017-08-21 11:09:35 -0400

    ex16: generate a random weak or strong password

---
+++ ex16.py
@@ -0,0 +1,26 @@
+# generate a random weak
+# or strong password
+
+import random
+import string
+
+def generateRandomPassword(charsChosen, size):
+    listOfChars = random.sample(charsChosen, size)
+    pwd = ''.join(listOfChars)
+    print pwd
+
+def main():
+    howStrong = raw_input("Do you want a weak password or a strong password? ")
+    howLong = int(raw_input("How many characters do you want the password to be? "))
+    if howStrong == "strong":
+        allChars = list(string.printable)
+        # removing unwanted characters
+        allChars.remove('\n'); allChars.remove('\t'); allChars.remove('\r'); allChars.remove('\x0b'); allChars.remove('\x0c')
+        allChars.remove('\\'); allChars.remove(' '); allChars.remove('`')
+        generateRandomPassword(allChars, howLong)
+
+    elif howStrong == "weak":
+        weakChars = string.ascii_letters
+        generateRandomPassword(weakChars, howLong)
+
+main()
