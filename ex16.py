eb79b287a15e1015cb362ccc080e7cbbb1c31d59
Abhijay Gupta <abhijay.gupta@ericsson.com>
2017-08-21 11:14:02 -0400

    ex16: added comments

--- ex16.py
+++ ex16.py
@@ -13,6 +13,8 @@ def main():
     howStrong = raw_input("Do you want a weak password or a strong password? ")
     howLong = int(raw_input("How many characters do you want the password to be? "))
     if howStrong == "strong":
+        # 'string.printable' gives all
+        # possible printable characters
         allChars = list(string.printable)
         # removing unwanted characters
         allChars.remove('\n'); allChars.remove('\t'); allChars.remove('\r'); allChars.remove('\x0b'); allChars.remove('\x0c')
@@ -20,6 +22,8 @@ def main():
         generateRandomPassword(allChars, howLong)
 
     elif howStrong == "weak":
+        # 'string.ascii_letters' gives all
+        # the alphabets, lowercase + uppercase
         weakChars = string.ascii_letters
         generateRandomPassword(weakChars, howLong)
 
